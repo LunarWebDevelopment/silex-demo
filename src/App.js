86c2ed90710daac1518e9739dbe1a7b1617aa931
Gary Odendaal <gary@lunarweb.co.za>
2020-05-20 15:57:16 +0200

    Question request process

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 class App extends Component {
+
+  state = {
+    currentClue: null,
+  }
+
+  getRandomClue = () => {
+    console.log("Function");
+    axios.get('http://jservice.io/api/random').then(res => {
+      this.setState({
+        currentClue: res.data,
+      })
+      console.log(this.state.currentClue);
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <button onClick={this.getRandomClue} type="button">Get Clue</button>
+         {this.state.currentClue && (
+           <p>{this.state.currentClue[0].question}</p>
+         )}
       </div>
     );
   }
